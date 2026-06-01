85b648e717808576b215b3ffc7b14ab29949f464
Fred Melo <fredfmelo@gmail.com>
2026-06-01 09:21:27 -0300

    removing fixed aws-region and adding actuator for health checks

--- src/main/java/com/fredfmelo/adminservice/config/AwsConfig.java
+++ src/main/java/com/fredfmelo/adminservice/config/AwsConfig.java
@@ -4,10 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.sns.SnsClient;
-import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class AwsConfig {
@@ -15,7 +13,6 @@ public class AwsConfig {
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.US_EAST_1)
                 .build();
     }
 
@@ -29,15 +26,6 @@ public class AwsConfig {
     @Bean
     public SnsClient snsClient() {
         return SnsClient.builder()
-                .region(Region.US_EAST_1)
                 .build();
     }
-
-    @Bean
-    public SqsClient sqsClient() {
-        return SqsClient.builder()
-                .region(Region.US_EAST_1)
-                .build();
-    }
-    
 }
